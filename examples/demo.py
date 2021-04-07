75701e623e2350a86a4387f7e1ffe5843a183ef7
Nathan <nathan@drezner.com>
2021-04-07 18:00:59 -0400

    Slight adjustments to demo

--- examples/demo.py
+++ examples/demo.py
@@ -2,8 +2,6 @@ import wikipedia_histories
 
 golden_swallow = wikipedia_histories.get_history("Golden swallow", include_text=False)
 
-print(golden_swallow)
-
 df = wikipedia_histories.to_df(golden_swallow)
 
-print(df.head())
+print(df)
