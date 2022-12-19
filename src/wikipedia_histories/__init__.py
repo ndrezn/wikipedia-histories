12964fea17442227e90865cec54920992b1d2701
Nathan Drezner <38958867+ndrezn@users.noreply.github.com>
2022-12-19 15:21:31 -0500

    Simplify dependencies (#15)
    * Simplify dependency tree by breaking out networks tool
    
    * Fix folder structure
    
    * Update README.md
    
    * Update description

---
+++ src/wikipedia_histories/__init__.py
@@ -0,0 +1,11 @@
+from .get_histories import (
+    get_history,
+    to_df,
+    get_text,
+    get_texts,
+    extract_lang_code_from_domain,
+)
+
+from .retrieve_metadata import get_metadata
+
+from . import networks
--- wikipedia_histories/__init__.py
+++
@@ -1,5 +0,0 @@
-from .get_histories import *
-from .network_builder import *
-from .retrieve_metadata import *
-from .analyze_networks import *
-from .get_category_articles import *
