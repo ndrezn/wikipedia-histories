e1245e69a3789f50b40cd2d39369b28b30e537b2
Nathan Drezner <38958867+ndrezn@users.noreply.github.com>
2022-12-19 16:12:25 -0500

    Burn in requirements.txt (#17)

--- setup.py
+++ setup.py
@@ -1,44 +1,29 @@
 # -*- coding: utf-8 -*-
-
-# python3 setup.py sdist bdist_wheel
-# twine upload --skip-existing dist/*
-
-import codecs
-import os
 import setuptools
 from setuptools import find_packages
-
-
-def local_file(file):
-    return codecs.open(os.path.join(os.path.dirname(__file__), file), "r", "utf-8")
-
-
-def read_req_file(req_type):
-    with open(f"requires-{req_type}.txt", encoding="utf-8") as fp:
-        requires = (line.strip() for line in fp)
-        return [req for req in requires if req and not req.startswith("#")]
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_reqs = [
-    line.strip()
-    for line in local_file("requirements.txt").readlines()
-    if line.strip() != ""
-]
-
 
 setuptools.setup(
     name="wikipedia_histories",
-    version="1.0.1",
+    version="1.1.0",
     author="Nathan Drezner",
     author_email="nathan@drezner.xyz",
     description="A Python tool to pull the complete edit history of a Wikipedia page",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ndrezn/wikipedia-histories",
-    install_requires=install_reqs,
+    install_requires=[
+        "aiohttp>=3.8.0",
+        "lxml==4.9.1",
+        "mwclient==0.10.1",
+        "mwparserfromhell==0.6",
+        "pandas>=1.2.3",
+        "wikipedia-api==0.5.4",
+    ],
     package_dir={"": "src"},
     packages=find_packages("src"),
     classifiers=[
@@ -46,8 +31,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    extras_require={
-        "networks": read_req_file("networks"),
-    },
+    extras_require={"networks": ["networkx>=2.6", "python-igraph==0.9.1"]},
     python_requires=">=3.6",
 )
